54af3dd16cbcb30b8c4bae7c802afc50b4857c60
Michael-RJ <22318054+Michael-RJ@users.noreply.github.com>
2023-12-14 13:27:02 +0000

    dependency upgrades

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,19 +3,18 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "uk.gov.hmrc"           %%  "webdriver-factory"  %  "0.45.0"   %  Test,
-    "org.scalatest"         %%  "scalatest"          %  "3.2.13"   %  Test,
-    "org.scalatestplus"     %%  "selenium-4-2"       %  "3.2.13.0" %  Test,
+    "uk.gov.hmrc"           %%  "webdriver-factory"  %  "0.46.0"   %  Test,
+    "org.scalatest"         %%  "scalatest"          %  "3.2.17"   %  Test,
+    "org.scalatestplus"     %%  "selenium-4-12"      %  "3.2.17.0" %  Test,
     "com.vladsch.flexmark"  %   "flexmark-all"       %  "0.62.2"   %  Test,
-    "io.cucumber"           %%  "cucumber-scala"     %  "8.7.0"    %  Test,
-    "io.cucumber"           %   "cucumber-junit"     %  "7.6.0"    %  Test,
-    "io.cucumber"           %   "cucumber-java"      %  "7.3.3"    %  Test,
+    "io.cucumber"           %%  "cucumber-scala"     %  "8.10.0"   %  Test,
+    "io.cucumber"           %   "cucumber-junit"     %  "7.15.0"   %  Test,
+    "io.cucumber"           %   "cucumber-java"      %  "7.15.0"   %  Test,
     "junit"                 %   "junit"              %  "4.13.2"   %  Test,
     "com.novocode"          %   "junit-interface"    %  "0.11"     %  Test,
-    "com.typesafe"          %   "config"             %  "1.4.2"    %  Test,
     "org.scalaj"            %%  "scalaj-http"        %  "2.4.2"    %  Test,
     "com.typesafe"          %   "config"             %  "1.4.2"    %  Test,
-    "com.typesafe.play"     %%  "play-json"          %  "2.9.2"    %  Test,
+    "com.typesafe.play"     %%  "play-json"          %  "2.10.3"   %  Test,
     "uk.gov.hmrc"           %%  "domain"             %  "8.3.0-play-28"
   )
 }
