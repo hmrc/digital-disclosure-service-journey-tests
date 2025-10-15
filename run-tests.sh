00eec7b655fb59d2e384c69e20d214f1e7e721a0
kreddam <194923482+kreddam@users.noreply.github.com>
2025-10-15 15:33:33 +0100

    DDCYLS-7521 Banner changes Fix update

--- run-tests.sh
+++ run-tests.sh
@@ -3,4 +3,4 @@
 BROWSER=$1
 ENVIRONMENT=$2
 
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Dbrowser.option.headless=false -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.runner.Runner" testReport
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.runner.Runner" testReport
