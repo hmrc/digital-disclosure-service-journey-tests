2c71b1009d54a28e3155027cc4df78a142adbea4
Kishore Reddy <194923482+kreddam@users.noreply.github.com>
2025-10-20 08:32:30 +0100

    DDCYLS-7278 PT runner update (#128)

--- run-tests.sh
+++ run-tests.sh
@@ -3,4 +3,4 @@
 BROWSER=$1
 ENVIRONMENT=$2
 
-sbt clean -Dbrowser="${BROWSER:=chrome}" -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.runner.Runner" testReport
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Dbrowser.usePreviousVersion=true -Denvironment="${ENVIRONMENT:=local}" "testOnly uk.gov.hmrc.runner.Runner" testReport
