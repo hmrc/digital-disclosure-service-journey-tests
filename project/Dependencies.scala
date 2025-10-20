3f5ff97d4f91210bbcbce4573a10a249a4231023
kreddam <194923482+kreddam@users.noreply.github.com>
2025-10-20 07:57:31 +0100

    DDCYLS-7278 PT runner update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "org.scalatest"         %%  "scalatest"          %  "3.2.19"   %  Test,
     "org.scalatestplus"     %%  "selenium-4-12"      %  "3.2.17.0" %  Test,
     "com.vladsch.flexmark"  %   "flexmark-all"       %  "0.64.8"   %  Test,
-    "uk.gov.hmrc"           %%  "ui-test-runner"     %  "0.45.0"   %  Test,
+    "uk.gov.hmrc"           %%  "ui-test-runner"     %  "0.50.0"   %  Test,
     "io.cucumber"           %%  "cucumber-scala"     %  "8.35.0"   %  Test,
     "io.cucumber"           %   "cucumber-junit"     %  "7.30.0"   %  Test,
     "io.cucumber"           %   "cucumber-java"      %  "7.30.0"   %  Test,
